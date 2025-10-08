fecc4fa24fb8bad0b3556380d5ee718868f2b02c
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-10-08 17:45:44 -0500

    added some packages in room-service

--- room-service/src/main/java/backend/roomservice/RoomServiceApplication.java
+++ room-service/src/main/java/backend/roomservice/RoomServiceApplication.java
@@ -2,8 +2,12 @@ package backend.roomservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class RoomServiceApplication {
 
     public static void main(String[] args) {
