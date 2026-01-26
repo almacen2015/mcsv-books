af94e8edd49dbaab05e785d6b6e4e3b82023c1fd
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-01-26 14:45:18 -0500

    Reser RoomControllerAdvice added

---
+++ reservation-service/src/main/java/backend/reservationservice/controllers/ReservationController.java
@@ -0,0 +1,9 @@
+package backend.reservationservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservations")
+public class ReservationController {
+}
